617e68c9ca832feacc988718f476db41d2e688c1
Niklas Wikström <niklas.wikstrom@gmail.com>
2022-11-07 13:47:50 +0100

    updated run script

--- run-server.sh
+++ run-server.sh
@@ -3,6 +3,15 @@
 LOG_DIR="logs"
 PORT=5080
 
+function already_running()
+{
+    if ss -tulpn | grep -q ":$PORT\>"
+    then
+        echo "Server seems already be running on port $PORT"
+        exit 1
+    fi
+}
+
 if [[ "$1" == stop ]]
 then
     SPID=$(<"$LOG_DIR/PID")
@@ -13,6 +22,7 @@ then
     exit 0
 fi
 
+already_running
 source venv/bin/activate
 ./server.py $PORT > $LOG_DIR/server.log 2>&1 &
 
